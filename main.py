5db0cf05d27d03164899b4712e83c68afcc9d634
Mamadou Moctar Ba <bmaxtar@gmail.com>
2021-01-22 22:59:20 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import discord
+import os 
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+  print('Nous nous sommes connectés en tant que {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+  if message.author == client.user:
+    return
+
+  if message.content.startSwith('$Hello'):
+    await message.channel.send('Hello !')
+
+client.run(os.getenv('TOKEN'))
+
