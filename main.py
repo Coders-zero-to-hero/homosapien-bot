abd307e88121188e1b9407393e7d700d20a8be0c
Mamadou Moctar Ba <bmaxtar@gmail.com>
2021-01-25 09:53:23 +0000

    Branch for test deployment

--- main.py
+++ main.py
@@ -1,7 +1,45 @@
 import discord
 import os 
+import requests 
+import json
+import random
+from replit import db
+
+
 
 client = discord.Client()
+
+sad_words = ["sad", "depressed", "unhappy", "angry", "miserable", "depressing"]
+
+starter_encouragements = [
+  "Cheer up !",
+  "Hang in there",
+  "You are a great person / bot!" 
+]
+
+def get_quote():
+  response = requests.get("https://zenquotes.io/api/random")
+  json_data = json.loads(response.text)
+  quote = json_data[0]['q'] + " - " + json_data[0]['a']
+  return(quote)
+
+def update_encouragements(encouraging_message):
+  if "encouragments" in db.key():
+    encouragements = db["encouragements"]
+    encouragements.append(encouraging_message)
+    db["encouragements"] = encouragements
+  
+  else:
+    db["encouragements"] = [encouraging_message]
+
+
+def delete_encouragment(index):
+  encouragements = db["encouragements"]
+  if len(encouragements) > index:
+    del encouragements[index]
+  db["encouragements"] = encouragements
+
+
 
 @client.event
 async def on_ready():
@@ -12,8 +50,14 @@ async def on_message(message):
   if message.author == client.user:
     return
 
-  if message.content.startswith('$hello'):
-    await message.channel.send('Hello !')
+  msg = message.content
+
+  if msg.startswith("$inspire"):
+    quote = get_quote()
+    await message.channel.send(quote)
+
+  if any(word in msg for word in sad_words):
+    await message.channel.send(random.choice(starter_encouragements))
 
 client.run(os.getenv('TOKEN'))
 
